776dee7a96b2be4ab0afbe0db9ab2b9f58b2015d
Illya Getmanchuk <ghostwolf012198@gmail.com>
2023-11-27 11:30:40 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,85 @@
-<template>
-  <router-view></router-view>
-</template>
-
-<script>
-
-export default {
-  name: 'App'
-}
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style lang="scss">
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-html, body {
-  scrollbar-color: rgba(#66615b, 0.8) rgba(#ffffff, 0);
-}
-::-webkit-scrollbar{
-  background-color: rgba(#ffffff, 0);
-  width: 5px;
-  height: 5px;
-}
-::-webkit-scrollbar-thumb{
-  border-radius: 5px;
-  background-color: rgba(#66615b, 0.8);
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
 }
 
-*{
-  margin: 0;
-  padding: 0;
-  -webkit-box-sizing: border-box;
-  -moz-box-sizing: border-box;
-  box-sizing: border-box;
-  scrollbar-width: thin;
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
 }
 
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
 </style>
