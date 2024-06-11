da5bb8de3a9e21f58a4e70bd38e33050dd2c9e49
Illya Getmanchuk <ghostwolf012198@gmail.com>
2024-06-11 15:31:55 +0300

    update github-page, google-cloud-kubernetes, config baseURL, add deploy, configmap

--- src/router/paths.ts
+++ src/router/paths.ts
@@ -1,9 +1,11 @@
-const cdnUrl = import.meta.env.PROD ? process.env.GITHUB_URL : '';
+import { useRuntimeConfig } from '#app';
+
+const baseURL = import.meta.env.PROD ? useRuntimeConfig().app.baseURL : '';
 
 export const routerPaths = {
-  home: cdnUrl + '/',
-  about: cdnUrl + '/about',
-  contacts: cdnUrl + '/contacts',
-  projects: cdnUrl + '/projects',
-  project: (link: string = ':link'): string => cdnUrl + `/project/${link}`,
+  home: baseURL + '/',
+  about: baseURL + '/about',
+  contacts: baseURL + '/contacts',
+  projects: baseURL + '/projects',
+  project: (link: string = ':link'): string => baseURL + `/project/${link}`,
 } as const;
